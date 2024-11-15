d8edfe945c8e45c122a58c32b857fd1dd1625e03
Asyrantheyt <94661066+Asyrantheyt@users.noreply.github.com>
2024-11-15 09:43:27 +0800

    Update Loader Auto.lua

--- Loader Auto.lua
+++ Loader Auto.lua
@@ -1,3 +1,5 @@
-if game.GameId == 331811267 then
+if game.PlaceId == 331811267 then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Asyrantheyt/Fairn-t-Hub/refs/heads/main/scripts/innovation%20space%20ship.lua"))()
+elseif game.PlaceId == 7466405192 then
+
 end
