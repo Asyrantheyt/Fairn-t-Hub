740c3cff9fb860bbe0ffd9cf36a64ce328595857
Asyrantheyt <94661066+Asyrantheyt@users.noreply.github.com>
2024-12-18 11:07:05 +0800

    Update Loader Auto.lua

--- Loader Auto.lua
+++ Loader Auto.lua
@@ -2,4 +2,6 @@ if game.PlaceId == 331811267 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/Asyrantheyt/Fairn-t-Hub/refs/heads/main/scripts/innovation%20space%20ship.lua"))()
 elseif game.GameId == 2907962276 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/Asyrantheyt/Fairn-t-Hub/refs/heads/main/scripts/sse2.lua"))()
+elseif game.GameId == 4380294826 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Asyrantheyt/Fairn-t-Hub/refs/heads/main/scripts/Black%20Hole%20Core.lua"))()
 end
