a33a37404f9c15143a2adab54c41553628c66dcb
Asyrantheyt <94661066+Asyrantheyt@users.noreply.github.com>
2024-12-18 18:24:55 +0800

    Update Loader Auto.lua

--- Loader Auto.lua
+++ Loader Auto.lua
@@ -4,4 +4,6 @@ elseif game.GameId == 2907962276 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/Asyrantheyt/Fairn-t-Hub/refs/heads/main/scripts/sse2.lua"))()
 elseif game.GameId == 4380294826 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/Asyrantheyt/Fairn-t-Hub/refs/heads/main/scripts/Black%20Hole%20Core.lua"))()
+elseif game.GameId == 4777817887 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Asyrantheyt/Fairn-t-Hub/refs/heads/main/scripts/Blade%20Ball.lua"))()
 end
