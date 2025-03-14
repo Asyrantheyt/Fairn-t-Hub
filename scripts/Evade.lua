406b5ada91b1632c7e2866f176471c629735b89b
Asyrantheyt <94661066+Asyrantheyt@users.noreply.github.com>
2025-03-14 08:40:29 +0800

    Update Evade.lua

--- scripts/Evade.lua
+++ scripts/Evade.lua
@@ -1,4 +1,4 @@
-local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/darkkcontrol/Roblox-Orion-UI-Libary-OP-UI-LIBARY-/refs/heads/main/source')))()
 local esp = false
 local botesp = false
 local instoption
